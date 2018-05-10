b221f7a2e51aae338393370279ab866ac1a13c85
esteban <echeverri.esteban@gmail.com>
2018-05-09 20:24:09 -0500

    Modificacion contructor our_cuda

--- anarray/our_cuda.py
+++ anarray/our_cuda.py
@@ -9,16 +9,16 @@ class OurCuda(AnArray,GpuArray):
     kernelBin = None
 
     #Se tiene que cambiar a self, n, m, Matrix, host=None
-    def __init__(self,Matrix,ready,n,m):
+    def __init__(self,n,m,Matrix = None,GpuMatrix = None):
         with open('kernel.cu') as file:
                 self.kernel_code_template = file.read()
         self.kernelBin = compiler.SourceModule(self.kernel_code_template)
         self.n = n
         self.m = m
-        if ready:
-            self.Matrix = Matrix
-        else:
+        if GpuMatrix is None:
             self.Matrix = gpuarray.to_gpu(Matrix)
+        else:
+            self.Matrix = Matrix
     
     def __del__(self):
         del(self.Matrix)
@@ -33,7 +33,7 @@ class OurCuda(AnArray,GpuArray):
             grid_size = 1
 
         Suma(np.int32(MATRIX_SIZE),self.Matrix, cudita.Matrix, c_gpu,block = (32, 32, 1), grid = (grid_size,grid_size,1))
-        return OurCuda(c_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,c_gpu)
     
     def subtract(self,cudita):
         Resta = self.kernelBin.get_function("Resta")
@@ -44,7 +44,7 @@ class OurCuda(AnArray,GpuArray):
         else:
             grid_size = 1
         Resta(np.int32(MATRIX_SIZE),self.Matrix, cudita.Matrix, c_gpu,block = (32, 32, 1), grid = (grid_size,grid_size,1))
-        return OurCuda(c_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,c_gpu)
 
     def multiply(self,cudita):
         Multi = self.kernelBin.get_function("Multi")
@@ -55,7 +55,7 @@ class OurCuda(AnArray,GpuArray):
         else:
             grid_size = 1
         Multi(np.int32(MATRIX_SIZE),self.Matrix, cudita.Matrix, c_gpu,block = (32, 32, 1), grid = (grid_size,grid_size,1))
-        return OurCuda(c_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,c_gpu)
 
     def divide(self,cudita):
         Divide = self.kernelBin.get_function("Divide")
@@ -66,7 +66,7 @@ class OurCuda(AnArray,GpuArray):
         else:
             grid_size = 1
         Divide(np.int32(MATRIX_SIZE),self.Matrix, cudita.Matrix, c_gpu,block = (32, 32, 1), grid = (grid_size,grid_size,1))
-        return OurCuda(c_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,c_gpu)
 
     def mod(self,cudita):
         Mod = self.kernelBin.get_function("Mod")
@@ -77,7 +77,7 @@ class OurCuda(AnArray,GpuArray):
         else:
             grid_size = 1
         Mod(np.int32(MATRIX_SIZE),self.Matrix, cudita.Matrix, c_gpu,block = (32, 32, 1), grid = (grid_size,grid_size,1))
-        return OurCuda(c_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,c_gpu)
       
     #Modificar la matriz original
     
@@ -139,7 +139,7 @@ class OurCuda(AnArray,GpuArray):
             grid_size = 1
 
         Cross(np.int32(MATRIX_SIZE),np.int32(self.m), np.int32(cudita.m),self.Matrix, cudita.Matrix, c_gpu, block = (32, 32, 1), grid = (grid_size,grid_size,1))
-        return OurCuda(c_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        OurCuda(self.n,cudita.m,None,c_gpu)
     
     #Better than Dom
     def domself(self):
@@ -160,7 +160,7 @@ class OurCuda(AnArray,GpuArray):
         else:
             grid_size = 1
         Dom(np.int32(MATRIX_SIZE), self.Matrix, b_gpu ,block = (1, 1024, 1), grid = (1,grid_size,1))
-        return OurCuda(b_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,b_gpu)
     
     def transpose(self):
         Transpose = self.kernelBin.get_function("Transpose")
@@ -171,7 +171,7 @@ class OurCuda(AnArray,GpuArray):
         else:
             grid_size = 1
         Transpose(np.int32(MATRIX_SIZE), self.Matrix, b_gpu ,block = (1, 1024, 1), grid = (1,grid_size,1))
-        return OurCuda(b_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,b_gpu)
 
     def diag(self):
         MATRIX_SIZE = self.n
@@ -182,7 +182,7 @@ class OurCuda(AnArray,GpuArray):
             grid_size = 1
         Diag = self.kernelBin.get_function("Diag")
         Diag(np.int32(MATRIX_SIZE), self.Matrix, b_gpu ,block = (1, 1024, 1), grid = (1,grid_size,1))
-        return OurCuda(b_gpu,True,1,MATRIX_SIZE)
+        return OurCuda(1,MATRIX_SIZE,None,b_gpu)
 
     def diagflat(self):
         MATRIX_SIZE = self.m
@@ -193,4 +193,4 @@ class OurCuda(AnArray,GpuArray):
             grid_size = 1
         DiagFlat = self.kernelBin.get_function("DiagFlat")
         DiagFlat(np.int32(MATRIX_SIZE), self.Matrix, b_gpu ,block = (1, 1024, 1), grid = (1,grid_size,1))
-        return OurCuda(b_gpu,True,MATRIX_SIZE,MATRIX_SIZE)
+        return OurCuda(MATRIX_SIZE,MATRIX_SIZE,None,b_gpu)
