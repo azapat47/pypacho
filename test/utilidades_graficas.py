df738fa04b811683bab64cb0828fcf08999b6123
diegoxfx <dcifuen3@gmail.com>
2019-01-03 23:26:40 -0500

    graficos de los resultados renovados

---
+++ test/utilidades_graficas.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def graficar(tabla, metodo='jacobi', x='tamaño', y='tiempo_promedio', plataformas=['cuda', 'opencl', 'numpy']):
+    colores = ['r', 'g', 'b']
+    
+    for indice, plataforma in enumerate(plataformas):
+        color = colores[indice]
+        tabla_plataforma = tabla.loc[tabla['plataforma'] == plataforma]
+        tabla_metodo = tabla_plataforma.loc[tabla_plataforma['metodo'] == metodo]
+        x_plataforma = tabla_metodo[x].values
+        y_plataforma = tabla_metodo[y].values
+        plt.plot(x_plataforma, y_plataforma, color=color, label=plataforma)
+    plt.title(y + ' vs ' + x + ' en ' + metodo)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+def describir(tabla):
+    promedio = tabla.mean()
+    std = tabla.std()
+    return [promedio, std]
